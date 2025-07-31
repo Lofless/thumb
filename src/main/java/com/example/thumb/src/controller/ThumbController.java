6c76cb82421ad86e14cabc58dc509c03fde20195
yangguang <3141219704@qq.com>
2025-07-31 14:06:41 +0800

    5.点赞

---
+++ src/main/java/com/example/thumb/src/controller/ThumbController.java
@@ -0,0 +1,26 @@
+package com.example.thumb.src.controller;
+
+import com.example.thumb.model.dto.thumb.DoThumbRequest;
+import com.example.thumb.src.common.BaseResponse;
+import com.example.thumb.src.common.ResultUtils;
+import com.example.thumb.src.service.ThumbService;
+import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/thumb")
+public class ThumbController {
+
+    @Resource
+    private ThumbService thumbService;
+
+    @PostMapping("/do")
+    public BaseResponse<Boolean> doThumb(@RequestBody DoThumbRequest doThumbRequest, HttpServletRequest request) {
+        Boolean success = thumbService.doThumb(doThumbRequest, request);
+        return ResultUtils.success(success);
+    }
+}
